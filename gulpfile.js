7f60598c35957ff66e388cd6a1d650443db53418
Robert Sefer <rsefer@gmail.com>
2014-12-13 10:35:10 -0600

    Cleanup

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,7 @@ var gulp          = require('gulp'),
     scsslint      = require('gulp-scss-lint'),
     autoprefixer  = require('gulp-autoprefixer'),
     jshint        = require('gulp-jshint'),
+    uglify        = require('gulp-uglify'),
     filter        = require('gulp-filter'),
     browserSync   = require('browser-sync');
 
@@ -14,39 +15,45 @@ gulp.task('browser-sync', function() {
   });
 });
 
-gulp.task('sass', function () {
-  return gulp.src('style/style.scss')
+gulp.task('scss-lint', function() {
+  return gulp.src('src/style/*/**.scss')
+    .pipe(scsslint());
+});
+
+gulp.task('sass', function() {
+  return gulp.src('src/style/*.scss')
     .pipe(sass({ outputStyle: 'compressed' }))
     .pipe(autoprefixer({
       browsers: ['last 10 versions', 'ie 8'],
       errLogToConsole: true,
       sync: true
     }))
-    .on('error', function (err) { console.log(err.message); })
-    .pipe(gulp.dest('style'))
+    .on('error', function(err) { console.log(err.message); })
+    .pipe(gulp.dest('dist/style'))
     .pipe(filter('**/*.css'))
     .pipe(browserSync.reload({
       stream: true
     }));
 });
 
-gulp.task('scss-lint', function() {
-  gulp.src('style/*/**.scss')
-    .pipe(scsslint());
-});
-
 gulp.task('lint', function() {
-  return gulp.src('scripts/*.js')
+  return gulp.src('src/scripts/*.js')
     .pipe(jshint())
     .pipe(jshint.reporter('default'));
 });
 
-gulp.task('bs-reload', function () {
+gulp.task('uglify', function() {
+  return gulp.src('src/scripts/*.js')
+    .pipe(uglify())
+    .pipe(gulp.dest('dist/scripts'));
+});
+
+gulp.task('bs-reload', function() {
   browserSync.reload();
 });
 
-gulp.task('default', ['sass', 'scss-lint', 'lint', 'browser-sync'], function () {
-  gulp.watch('style/*/**.scss', ['sass', 'scss-lint']);
-  gulp.watch('scripts/*.js', ['lint']);
+gulp.task('default', ['scss-lint', 'sass', 'uglify', 'lint', 'browser-sync'], function() {
+  gulp.watch('src/style/*/**.scss', ['scss-lint', 'sass']);
+  gulp.watch('src/scripts/*.js', ['lint', 'uglify']);
   gulp.watch('*.html', ['bs-reload']);
 });
